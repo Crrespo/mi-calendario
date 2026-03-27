073ab7915309d68b24eb15fc41498ad32bea9de2
Crrespo <crrrespo@gmail.com>
2026-03-27 20:28:10 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'dayflow-v2';
+const ASSETS = [
+  '/mi-calendario/',
+  '/mi-calendario/index.html',
+  '/mi-calendario/manifest.json',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,300;0,400;0,500;1,400&family=DM+Mono:wght@400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request)
+        .catch(() => caches.match('/mi-calendario/'))
+      )
+  );
+});
